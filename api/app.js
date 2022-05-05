70cfe5dd72dbaf76e69b5fbf92409a87f7395329
rafik12345 <rafik-08@hotmail.com>
2022-05-05 21:08:24 +0200

    connect to DB && server start function

--- api/app.js
+++ api/app.js
@@ -1,10 +1,25 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect');
 
 const taskRouter = require('./routes/task');
 
 app.use('/api/v1/tasks', taskRouter)
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000...')
-})
+const port = process.env.MONGO_PORT || 5000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
