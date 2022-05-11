c4c478b6d095f745b4fde337b5090b0bb931bdf7
Rafik <rafikbdev@gmail.com>
2022-05-11 11:43:54 +0200

    Add UnauthenticatedError ++ updated error-handler middleware

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors/index');
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body })
@@ -12,17 +13,17 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     const {email, password} = req.body;
     if(!email || !password) {
-        return res.json('Please provide email and password')
+        throw new BadRequestError('Please provide email and password')
     }
 
     const user = await User.findOne({email});
     if(!user){
-        return res.json('Invalid Credentials')
+        throw new UnauthenticatedError('Invalid Credentials')
     }
 
     const isCorrectPass = await user.comparePassword(password);
     if(!isCorrectPass){
-        return res.json('Invalid Credentials')
+        throw new UnauthenticatedError('Invalid Credentials')
     }
 
     const token = user.createJWT();
--- api/middleware/error-handler.js
+++ api/middleware/error-handler.js
@@ -6,6 +6,20 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         msg: err.message || 'Something went wrong try again later'
     }
 
+    if(err.name === 'ValidationError'){
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(', ')
+        customError.statusCode = 404
+    }
+
+    if(err.code === 11000){
+        customError.msg = `Duplication Error: ${Object.keys(
+            err.keyValue
+            )}, please provide anohther value`
+        customError.statusCode = 400
+    }
+
     if(err.name === 'CastError'){
         customError.msg = `No item found with id: ${err.value}`;
         customError.statusCode = 404;
