c0cabf922870e263dcc4b4d9890ad21e496a2e15
Rafik <rafikbdev@gmail.com>
2022-05-11 11:17:12 +0200

    Login user & create JWT server

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,13 +3,30 @@ const { StatusCodes } = require('http-status-codes');
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body })
+    const token = user.createJWT();
     res
         .status(StatusCodes.CREATED)
-        .json(user)
+        .json({user: {name: user.getName()}, token})
 }
 
-const login = (req, res) => {
-    res.json({msg: 'login'})
+const login = async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password) {
+        return res.json('Please provide email and password')
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+        return res.json('Invalid Credentials')
+    }
+
+    const isCorrectPass = await user.comparePassword(password);
+    if(!isCorrectPass){
+        return res.json('Invalid Credentials')
+    }
+
+    const token = user.createJWT();
+    res.json({user: {name: user.getName()}, token})
 }
 
 module.exports = {
